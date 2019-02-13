ff2dfb63f84579b2f5b5fef90f1b7234f08b9dec
Philipp Schmid <philipp@Philipps-MacBook-Pro.local>
2019-02-13 09:50:01 +0100

    added gitignore an changed index

--- index.ts
+++ index.ts
@@ -1,15 +1,11 @@
 /** @format */
 
-// Copyright (c) Alex Ellis 2017. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
 import * as express from 'express'
 import {ApolloServer} from 'apollo-server-express'
 import {typeDefs} from './function/typeDefs'
 import resolvers from './function/resolvers'
 const app = express()
 require('dotenv').config({path: `./function/.env.${process.env.NODE_ENV}`})
-
 
 const server = new ApolloServer({
   typeDefs,
